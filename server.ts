99eb89c47c913a627668b63ca51acba588e35d70
UmeshpJadhav <Umesh2005jadhav@gmail.com>
2025-02-13 12:12:11 +0530

    setup express serve

---
+++ server.ts
@@ -0,0 +1,17 @@
+import app from "./src/app";
+
+
+const startServer = () => {
+    const PORT = process.env.PORT || 3000;
+
+
+   app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+   })
+};
+
+
+
+
+startServer();
+
